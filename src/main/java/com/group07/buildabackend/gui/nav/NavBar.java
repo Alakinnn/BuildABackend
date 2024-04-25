94f1abdbc398c8d37b07448ce09d2466d4de03d1
MasterMobo <mastermobo101@gmail.com>
2024-04-25 16:48:40 +0700

    added responsive to navbar

--- src/main/java/com/group07/buildabackend/gui/nav/NavBar.java
+++ src/main/java/com/group07/buildabackend/gui/nav/NavBar.java
@@ -2,8 +2,13 @@ package com.group07.buildabackend.gui.nav;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -15,6 +20,7 @@ public class NavBar implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        navBox.setBackground(new Background(new BackgroundFill(Color.LIGHTSKYBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
         // TODO: add actual navigation
         Node[] sampleNodes = {new Text("Nav 1"), new Text("Nav 2"), new Text("Nav 3")};
         navBox.getChildren().addAll(sampleNodes);

44ec3776423f1de810880051b7ab15204052187e
Alakinnn <s3978452@rmit.edu.vn>
2024-04-29 02:11:36 +0700

    upldate policy holder

--- src/main/java/com/group07/buildabackend/backend/model/customer/PolicyHolder.java
+++ src/main/java/com/group07/buildabackend/backend/model/customer/PolicyHolder.java
@@ -1,4 +1,9 @@
 package com.group07.buildabackend.backend.model.customer;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PolicyHolder")
 public class PolicyHolder extends Customer implements Beneficiary {
 }

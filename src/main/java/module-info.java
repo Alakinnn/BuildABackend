b35acf8be2491dab68c40ba4acd3043832a91ccf
MasterMobo <mastermobo101@gmail.com>
2024-05-03 16:14:31 +0700

    added jakarta to modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.group07.buildabackend {
     requires javafx.controls;
     requires javafx.fxml;
-
-
+    requires jakarta.persistence;
+    
     opens com.group07.buildabackend to javafx.fxml;
     exports com.group07.buildabackend;
     exports com.group07.buildabackend.gui;

bbbc9c5ec187c66beabb4cfc81e530f5c6a6c03d
MasterMobo <mastermobo101@gmail.com>
2024-05-10 19:23:30 +0700

    added profile page redirect

--- src/main/java/com/group07/buildabackend/gui/components/user/UserHyperlink.java
+++ src/main/java/com/group07/buildabackend/gui/components/user/UserHyperlink.java
@@ -1,7 +1,10 @@
 package com.group07.buildabackend.gui.components.user;
 
 import com.group07.buildabackend.backend.model.SystemUser;
+import com.group07.buildabackend.gui.SceneManager;
 import com.group07.buildabackend.gui.components.Component;
+import com.group07.buildabackend.gui.pages.Page;
+import com.group07.buildabackend.gui.pages.user.UserProfilePage;
 import javafx.scene.control.Hyperlink;
 
 public class UserHyperlink extends Component {
@@ -9,6 +12,8 @@ public class UserHyperlink extends Component {
         Hyperlink link = new Hyperlink(user.getFullName() + " (" + user.getUserId() + ")");
         link.setOnAction(event -> {
             // TODO: redirect to profile page
+            Page page = new UserProfilePage(user.getUserId());
+            SceneManager.getInstance().switchToPage(page);
         });
 
         root = link;

5074393c50b6e4b1595e2382b35c2a202224b738
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2024-05-12 15:07:21 +0700

    Refactor PolicyOwnerController based on the change of CalculateService

--- src/main/java/com/group07/buildabackend/backend/controller/PolicyOwnerController.java
+++ src/main/java/com/group07/buildabackend/backend/controller/PolicyOwnerController.java
@@ -3,9 +3,8 @@ package com.group07.buildabackend.backend.controller;
 import com.group07.buildabackend.backend.dto.InsuranceCostDTO;
 import com.group07.buildabackend.backend.service.policyOwnerService.CalculateService;
 
-// TODO: MOVE THIS CODE BLOCK CONTENT TO A SERVICE CLASS
 public class PolicyOwnerController extends Controller{
-    public double calculateAnnualCost(InsuranceCostDTO insuranceCostDTO){
+    public Response<Double> calculateAnnualCost(InsuranceCostDTO insuranceCostDTO){
         return CalculateService.calculateAnnualCost(insuranceCostDTO);
     }
 }

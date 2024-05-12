89dbc5690b5d5d5b923f6e2a73eebdbd19b7399f
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2024-05-12 10:29:15 +0700

    Refactor PolicyOwnerController

--- src/main/java/com/group07/buildabackend/backend/controller/PolicyOwnerController.java
+++ src/main/java/com/group07/buildabackend/backend/controller/PolicyOwnerController.java
@@ -1,34 +1,11 @@
 package com.group07.buildabackend.backend.controller;
 
 import com.group07.buildabackend.backend.dto.InsuranceCostDTO;
-import com.group07.buildabackend.backend.model.SystemUserType;
-import com.group07.buildabackend.backend.model.customer.Beneficiary;
-import com.group07.buildabackend.backend.model.customer.Customer;
-import com.group07.buildabackend.backend.model.customer.PolicyOwner;
-import com.group07.buildabackend.backend.repository.PolicyOwnerRepository;
-
-import java.util.List;
-
+import com.group07.buildabackend.backend.service.policyOwnerService.CalculateService;
 
 // TODO: MOVE THIS CODE BLOCK CONTENT TO A SERVICE CLASS
 public class PolicyOwnerController extends Controller{
-    public double calculateAnnualCost(InsuranceCostDTO dto){
-        final double dependentCost = 0.6;
-
-        PolicyOwnerRepository repository = new PolicyOwnerRepository();
-        PolicyOwner po = repository.retrieveActorById(dto.getId());
-
-        double yearlyRate = po.getYearlyRate();
-
-        int holderCount = 0, dependentCount = 0;
-
-        List<Beneficiary> beneficiaries = repository.retrieveAllBeneficiary(dto.getId());
-        for(Customer customer: beneficiaries){
-            SystemUserType customerType = customer.getUserType();
-            if(customerType == SystemUserType.policy_holder){holderCount ++;}
-            if(customerType == SystemUserType.dependent){dependentCount ++;}
-        }
-
-        return holderCount * yearlyRate + dependentCount * yearlyRate * dependentCost;
+    public double calculateAnnualCost(InsuranceCostDTO insuranceCostDTO){
+        return CalculateService.calculateAnnualCost(insuranceCostDTO);
     }
 }

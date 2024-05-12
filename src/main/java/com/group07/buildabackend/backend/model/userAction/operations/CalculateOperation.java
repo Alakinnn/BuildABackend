5283dcedcb0e04d1897b24ffc755c4eadfd3e1c4
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2024-05-12 16:31:26 +0700

    Add AnnualCostAction

---
+++ src/main/java/com/group07/buildabackend/backend/model/userAction/operations/CalculateOperation.java
@@ -0,0 +1,6 @@
+package com.group07.buildabackend.backend.model.userAction.operations;
+
+public class CalculateOperation implements OperationType{
+    @Override
+    public String getDescription() {return "Calculate ";}
+}

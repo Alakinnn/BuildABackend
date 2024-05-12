fb8480fb696c93939640546737bc82aa627c259b
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2024-05-12 15:06:46 +0700

    Refactor CalculateService to handle Exception

--- src/main/java/com/group07/buildabackend/backend/service/policyOwnerService/CalculateService.java
+++ src/main/java/com/group07/buildabackend/backend/service/policyOwnerService/CalculateService.java
@@ -1,30 +1,50 @@
 package com.group07.buildabackend.backend.service.policyOwnerService;
 
+import com.group07.buildabackend.backend.controller.Response;
 import com.group07.buildabackend.backend.dto.InsuranceCostDTO;
 import com.group07.buildabackend.backend.model.SystemUserType;
 import com.group07.buildabackend.backend.model.customer.Beneficiary;
 import com.group07.buildabackend.backend.model.customer.Customer;
 import com.group07.buildabackend.backend.model.customer.PolicyOwner;
-import com.group07.buildabackend.backend.repository.PolicyOwnerRepository;
+import com.group07.buildabackend.backend.service.Service;
+import com.group07.buildabackend.backend.validation.customExceptions.InvalidInputException;
 
 import java.util.List;
 
-public class CalculateService {
-    public static double calculateAnnualCost(InsuranceCostDTO dto){
+public class CalculateService extends Service {
+    public static Response<Double> calculateAnnualCost(InsuranceCostDTO dto){
+        Response<Double> response = new Response(null);
         final double dependentCost = 0.6;
 
-        PolicyOwnerRepository repository = new PolicyOwnerRepository();
-        PolicyOwner po = repository.retrieveActorById(dto.getId());
+        try{
+            PolicyOwner po = policyOwnerRepository.retrieveActorById(dto.getId());
+
+            if(po == null){
+                throw new InvalidInputException("Policy owner not found", 400);
+            }
+
+            List<Beneficiary> beneficiaries = policyOwnerRepository.retrieveAllBeneficiary(dto.getId());
+            double annualCost = getAnnualCost(beneficiaries, po, dependentCost);
+
+            handleSuccess(response, "Successfully calculated annual cost", 200, annualCost);
+        } catch(InvalidInputException e){
+            handleException(response, e.getMessage(), e.getErrorCode());
+        }
+        return response;
+    }
+
+    private static double getAnnualCost(List<Beneficiary> beneficiaries, PolicyOwner po, double dependentCost) throws InvalidInputException {
+        if(beneficiaries == null){
+            throw new InvalidInputException("Beneficiaries not found", 400);
+        }
 
         double yearlyRate = po.getYearlyRate();
-
         int holderCount = 0, dependentCount = 0;
 
-        List<Beneficiary> beneficiaries = repository.retrieveAllBeneficiary(dto.getId());
         for(Customer customer: beneficiaries){
             SystemUserType customerType = customer.getUserType();
-            if(customerType == SystemUserType.policy_holder){holderCount ++;}
-            if(customerType == SystemUserType.dependent){dependentCount ++;}
+            if (customerType == SystemUserType.policy_holder) {holderCount ++;}
+            if (customerType == SystemUserType.dependent) {dependentCount ++;}
         }
 
         return holderCount * yearlyRate + dependentCount * yearlyRate * dependentCost;

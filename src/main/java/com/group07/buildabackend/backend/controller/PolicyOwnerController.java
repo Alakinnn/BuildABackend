c7907206e98ac5d7d46a7b1dc854018114a8e8c0
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2024-05-13 13:41:03 +0700

    Implement update users into PolicyOwnerController

--- src/main/java/com/group07/buildabackend/backend/controller/PolicyOwnerController.java
+++ src/main/java/com/group07/buildabackend/backend/controller/PolicyOwnerController.java
@@ -1,10 +1,24 @@
 package com.group07.buildabackend.backend.controller;
 
 import com.group07.buildabackend.backend.dto.InsuranceCostDTO;
+import com.group07.buildabackend.backend.dto.beneficiaryDTO.DependentDTO;
+import com.group07.buildabackend.backend.dto.beneficiaryDTO.PolicyHolderDTO;
+import com.group07.buildabackend.backend.model.customer.Dependent;
+import com.group07.buildabackend.backend.model.customer.PolicyHolder;
 import com.group07.buildabackend.backend.service.policyOwnerService.CalculateService;
+import com.group07.buildabackend.backend.service.policyOwnerService.UpdateDependentService;
+import com.group07.buildabackend.backend.service.policyOwnerService.UpdatePolicyHolderService;
 
 public class PolicyOwnerController extends Controller{
     public Response<Double> calculateAnnualCost(InsuranceCostDTO insuranceCostDTO){
         return CalculateService.calculateAnnualCost(insuranceCostDTO);
+    }
+
+    public Response<PolicyHolder> updatePolicyHolder(PolicyHolderDTO policyHolderDTO){
+        return UpdatePolicyHolderService.updatePolicyHolder(policyHolderDTO);
+    }
+
+    public Response<Dependent> updateDependent(DependentDTO dependentDTO){
+        return UpdateDependentService.policyOwnerUpdateDependent(dependentDTO);
     }
 }

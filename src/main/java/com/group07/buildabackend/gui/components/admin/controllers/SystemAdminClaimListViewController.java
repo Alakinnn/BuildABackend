fdb169d250a4a96fe4fcd3ed3b3f23ec323e9173
MasterMobo <mastermobo101@gmail.com>
2024-05-18 12:48:01 +0700

    added successful claim amount to admin

--- src/main/java/com/group07/buildabackend/gui/components/admin/controllers/SystemAdminClaimListViewController.java
+++ src/main/java/com/group07/buildabackend/gui/components/admin/controllers/SystemAdminClaimListViewController.java
@@ -1,7 +1,9 @@
 package com.group07.buildabackend.gui.components.admin.controllers;
 
+import com.group07.buildabackend.backend.controller.AdminController;
 import com.group07.buildabackend.backend.controller.InsuranceClaimController;
 import com.group07.buildabackend.backend.controller.Response;
+import com.group07.buildabackend.backend.dto.queryDTO.ClaimQueryDTO;
 import com.group07.buildabackend.backend.model.insuranceClaim.InsuranceClaim;
 import com.group07.buildabackend.gui.components.ComponentController;
 import com.group07.buildabackend.gui.components.claim.ClaimList;
@@ -9,12 +11,15 @@ import com.group07.buildabackend.gui.tasks.TaskRunner;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class SystemAdminClaimListViewController implements Initializable, ComponentController {
+    @FXML
+    private Text successAmount;
     @FXML
     private HBox claimsContainer;
 
@@ -30,6 +35,11 @@ public class SystemAdminClaimListViewController implements Initializable, Compon
 
         TaskRunner<List<InsuranceClaim>> runner = new TaskRunner<>(this::fetchClaims, claims -> {
             this.claims.addAllClaims(claims);
+
+            TaskRunner<Double> statsRunner = new TaskRunner<>(this::fetchSuccessAmount, successAmount -> {
+                this.successAmount.setText(String.valueOf(successAmount));
+            });
+            statsRunner.run();
         });
         runner.run();
     }
@@ -38,5 +48,10 @@ public class SystemAdminClaimListViewController implements Initializable, Compon
         InsuranceClaimController controller = new InsuranceClaimController();
         Response<List<InsuranceClaim>> res = controller.retrieveAll();
         return res.getData();
+    }
+
+    private Double fetchSuccessAmount() {
+        AdminController controller = new AdminController();
+        return controller.calculateSuccessfulClaimsAmount().getData();
     }
 }

7ffd499f76e1ebe0b674518e692660d2f51bb86a
MasterMobo <mastermobo101@gmail.com>
2024-05-18 19:06:51 +0700

    fixed some build errors

--- src/main/java/com/group07/buildabackend/backend/cloudinary/CloudinaryConnectionManager.java
+++ src/main/java/com/group07/buildabackend/backend/cloudinary/CloudinaryConnectionManager.java
@@ -8,12 +8,11 @@ import com.cloudinary.*;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class CloudinaryConnectionManager {
-    protected static final Dotenv dotenv = Dotenv.load();
     private static Cloudinary cloudinary;
 
     public static Cloudinary getCloudinary() {
         if (cloudinary == null) {
-            cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+            cloudinary = new Cloudinary("cloudinary://799347643284176:YsdI4sZ6ULhp5A1P_KI8Biz6bqg@dhfh7z051");
             cloudinary.config.secure = true;
         }
         return cloudinary;
